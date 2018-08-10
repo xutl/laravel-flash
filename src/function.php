1806c34453dab4ad73974d7d6249f68f65d3cf70
XuTongle <xutongle@gmail.com>
2018-08-10 19:06:53 +0800

    update

--- src/function.php
+++ src/function.php
@@ -5,10 +5,13 @@ if (!function_exists('flash')) {
 
     /**
      * @param string $message
-     * @return mixed
+     * @return \XuTL\Flash\FlashNotifiy
      */
     function flash($message = '')
     {
+        /**
+         * @var \XuTL\Flash\FlashNotifiy $notify
+         */
         $notify = app('flash');
         if (!is_null($message)) {
             return $notify->success($message); //默认为success
